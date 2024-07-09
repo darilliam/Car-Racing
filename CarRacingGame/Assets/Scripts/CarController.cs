802f922b7effa807151c825355e527b141d6aa28
Daria Volodenko <dasha.volodenko@gmail.com>
2024-07-10 00:39:32 +0200

    Add Ackermann steering formula and update input mechanics

--- CarRacingGame/Assets/Scripts/CarController.cs
+++ CarRacingGame/Assets/Scripts/CarController.cs
@@ -4,49 +4,93 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    internal enum DriveType
+    {
+        frontWheelDrive,
+        rearWheelDrive,
+        allWheelDrive
+    }
+    [SerializeField]private DriveType driveType;
+
     // Array of wheel colliders
     public WheelCollider[] wheels = new WheelCollider[4];
     public GameObject[] wheelMesh = new GameObject[4];
     public int motorTorque = 100;
     public float steeringMax = 25;
+
+    [Header("Car Specs")]
+    public float wheelBase; // in meters
+    public float rearTrack; // in meters
+    public float turnRadius; // in meters
+
+    //public float ackermannAngleLeft;
+    //public float ackermannAngleRight;
+
+
+    private InputManager inputManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Getobjects();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
         AnimateWheel();
+        CarMovement();
+        SteerCar();
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    private void CarMovement()
+    {
+        //float totalPower;
+
+        if (driveType == DriveType.allWheelDrive)
         {
-            for(int i = 0; i < wheels.Length; i++)
+            for (int i = 0; i < wheels.Length; i++)
             {
-                wheels[i].motorTorque = motorTorque;
+                wheels[i].motorTorque = inputManager.vertical * (motorTorque / 4);
             }
         }
-
-        if(Input.GetAxis("Horizontal") != 0)
+        else if (driveType == DriveType.rearWheelDrive)
         {
-            for (int i = 0; i < wheels.Length -2; i++)
+            for (int i = 2; i < wheels.Length; i++)
             {
-                wheels[i].steerAngle = Input.GetAxis("Horizontal") * steeringMax;
+                wheels[i].motorTorque = inputManager.vertical * (motorTorque / 2);
             }
         }
         else
         {
             for (int i = 0; i < wheels.Length - 2; i++)
             {
-                wheels[i].steerAngle = 0;
+                wheels[i].motorTorque = inputManager.vertical * (motorTorque / 2);
             }
         }
+    }
+
+    private void SteerCar()
+    {
+        // The Ackermann steering formula calculates the correct steering angles for the wheels of a vehicle to ensure
+        // they follow concentric circles, reducing tire wear and improving handling during turns.
+
+        if(inputManager.horizontal > 0) // is turning right
+        {
+            wheels[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * inputManager.horizontal;
+            wheels[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * inputManager.horizontal;
+        }
+        else if(inputManager.horizontal < 0) // is turning left
+        {
+            wheels[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * inputManager.horizontal;
+            wheels[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * inputManager.horizontal;
+        }
+        else
+        {
+            wheels[0].steerAngle = 0;
+            wheels[1].steerAngle = 0;
+        }
+
     }
 
     private void AnimateWheel()
@@ -60,5 +104,10 @@ public class CarController : MonoBehaviour
             wheelMesh[i].transform.position = wheelPosition;
             wheelMesh[i].transform.rotation = wheelRotation;
         }
+    }
+
+    private void Getobjects()
+    {
+        inputManager = GetComponent<InputManager>();
     }
 }
