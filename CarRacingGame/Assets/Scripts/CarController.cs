7cde18f3627fec8dad2511efde3c274b5ad9df99
Daria Volodenko <dasha.volodenko@gmail.com>
2024-08-16 01:14:00 +0200

    Add Drift Controller with small bug

--- CarRacingGame/Assets/Scripts/CarController.cs
+++ CarRacingGame/Assets/Scripts/CarController.cs
@@ -11,8 +11,7 @@ public class CarController : MonoBehaviour
         rearWheelDrive,
         allWheelDrive
     }
-    [SerializeField]
-    private DriveType driveType;
+    [SerializeField] private DriveType driveType;
 
     [Serializable]
     public struct Wheel
@@ -28,25 +27,32 @@ public class CarController : MonoBehaviour
     public float steeringMax = 25;
 
     [Header("Car Specs")]
-    public float wheelBase; // in meters
-    public float rearTrack; // in meters
-    public float turnRadius; // in meters
-    public float downForceValue = 50;
+    [SerializeField] private float wheelBase; // in meters
+    [SerializeField] private float rearTrack; // in meters
+    [SerializeField] private float turnRadius; // in meters
+    [SerializeField] private float maxSpeedKPH;
+    [SerializeField] private float downForceValue = 50.0f;
     // public float handbrakePower;
-    public float brakeForce;
+    [SerializeField] private float brakeAcceleration = 50.0f;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private Transform _centerOfMass;
+
+    [Header("Variables")]
+    [SerializeField] private float handBrakeFrictionMultiplier = 2f;
 
 
+    [Header("DEBUG")]
     // Kilometres per hour
     public float KPH;
-
-    public float[] slip = new float[4];
+    [SerializeField] private float[] slip = new float[4];
 
     private InputManager _inputManager;
     private Rigidbody _carRb;
     private float _brakeInput;
+    private WheelFrictionCurve _forwardFriction;
+    private WheelFrictionCurve _sidewaysFriction;
+    private float _driftFactorValue;
 
-    [SerializeField]
-    private Transform _centerOfMass;
 
     // Start is called before the first frame update
     void Start()
@@ -64,6 +70,8 @@ public class CarController : MonoBehaviour
         SteerCar();
         Friction();
         CheckInput();
+        LimitMaxSpeed();
+        AdjustTraction();
     }
 
     private void CarMovement()
@@ -107,6 +115,18 @@ public class CarController : MonoBehaviour
         */
     }
 
+    private void LimitMaxSpeed()
+    {
+        // Current speed in kilometres per hour
+        float currentSpeedKPH = _carRb.velocity.magnitude * 3.6f;
+
+        if (currentSpeedKPH > maxSpeedKPH)
+        {
+            // Limit speed by reducing forward force
+            _carRb.velocity = (_carRb.velocity.normalized * maxSpeedKPH) / 3.6f;
+        }
+    }
+
     private void SteerCar()
     {
         // The Ackermann steering formula calculates the correct steering angles for the wheels of a vehicle to ensure
@@ -147,10 +167,97 @@ public class CarController : MonoBehaviour
             {
                 wheel.wheelCollider.brakeTorque = brakeForce * _brakeInput * 0.3f;
             }
+
+            if(_inputManager.brakePedal)
+            {
+                wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+            }
+            else
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
         }
     }
 
-    
+    private void AdjustTraction()
+    {
+        // Time it takes to go from normal drive to drift
+        float driftSmothFactor = .7f * Time.deltaTime;
+
+        // If the brake pedal is depressed
+        if (_inputManager.brakePedal)
+        {
+            // Save the current friction parameters
+            _sidewaysFriction = wheels[0].wheelCollider.sidewaysFriction;
+            _forwardFriction = wheels[0].wheelCollider.forwardFriction;
+
+            if (_inputManager.horizontal > 0)
+            {
+                Debug.Log("R");
+            }
+            else if (_inputManager.horizontal < 0)
+            {
+                Debug.Log("L");
+            }
+
+            float velocity = 0;
+            // Smoothly change the friction parameters for drifting
+            _sidewaysFriction.extremumValue = _sidewaysFriction.asymptoteValue = _forwardFriction.extremumValue = _forwardFriction.asymptoteValue =
+                Mathf.SmoothDamp(_forwardFriction.asymptoteValue, _driftFactorValue * handBrakeFrictionMultiplier,ref velocity ,driftSmothFactor );
+
+            // Apply the modified friction parameters to all wheels
+            for (int i = 0; i < 4; i++) {
+                wheels[i].wheelCollider.sidewaysFriction = _sidewaysFriction;
+                wheels[i].wheelCollider.forwardFriction = _forwardFriction;
+            }
+
+            // Increase traction for the front wheels
+            _sidewaysFriction.extremumValue = _sidewaysFriction.asymptoteValue = _forwardFriction.extremumValue = _forwardFriction.asymptoteValue =  1.1f;
+
+            // Extra grip for the front wheels
+            for (int i = 0; i < 2; i++) {
+                wheels[i].wheelCollider.sidewaysFriction = _sidewaysFriction;
+                wheels[i].wheelCollider.forwardFriction = _forwardFriction;
+            }
+
+            _carRb.AddForce(transform.forward * (KPH / 400) * 40000 );
+
+            Vector3 driftForceValue = transform.forward * (KPH / 400) * 40000;
+            Debug.Log("Drift Force Applied: " + driftForceValue);
+            Debug.Log("Horizontal Input: " + _inputManager.horizontal);
+            Debug.Log("Vertical Input: " + _inputManager.vertical);
+        }
+        // Executed when handbrake is being held
+        else
+        {
+			_forwardFriction = wheels[0].wheelCollider.forwardFriction;
+			_sidewaysFriction = wheels[0].wheelCollider.sidewaysFriction;
+
+			_forwardFriction.extremumValue = _forwardFriction.asymptoteValue = _sidewaysFriction.extremumValue = _sidewaysFriction.asymptoteValue = 
+                ((KPH * handBrakeFrictionMultiplier) / 300) + 1;
+
+			for (int i = 0; i < 4; i++) {
+				wheels[i].wheelCollider.forwardFriction = _forwardFriction;
+				wheels[i].wheelCollider.sidewaysFriction = _sidewaysFriction;
+
+			}
+        }
+
+            //checks the amount of slip to control the drift
+		for(int i = 2;i<4 ;i++){
+
+            WheelHit wheelHit;
+
+            wheels[i].wheelCollider.GetGroundHit(out wheelHit);            
+
+			if(wheelHit.sidewaysSlip < 0 ) _driftFactorValue = (1 + -_inputManager.horizontal) * Mathf.Abs(wheelHit.sidewaysSlip) ;
+
+			if(wheelHit.sidewaysSlip > 0 ) _driftFactorValue = (1 + _inputManager.horizontal )* Mathf.Abs(wheelHit.sidewaysSlip );
+		}
+    }
+
+
+
     private void CheckInput()
     {
         float movingDirectional = Vector3.Dot(transform.forward, _carRb.velocity);
@@ -197,6 +304,6 @@ public class CarController : MonoBehaviour
     {
         _inputManager = GetComponent<InputManager>();
         _carRb = GetComponent<Rigidbody>();
-        _carRb.centerOfMass = _centerOfMass.position;
+        _carRb.centerOfMass = _centerOfMass.localPosition;
     }
 }
--- CarRacingGame/Assets/Scripts/InputManager.cs
+++ CarRacingGame/Assets/Scripts/InputManager.cs
@@ -6,13 +6,15 @@ public class InputManager : MonoBehaviour
 {
     public float vertical;
     public float horizontal;
-    public bool handbrake;
+    // public bool handbrake;
+    public bool brakePedal;
 
     private void FixedUpdate()
     {
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
 
-        handbrake = (Input.GetAxis("Jump") != 0)? true : false;
+        // handbrake = (Input.GetAxis("Jump") != 0)? true : false;
+        brakePedal = (Input.GetKey(KeyCode.Space)) ? true : false;
     }
 }
